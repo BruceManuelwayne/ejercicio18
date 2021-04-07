96946cbc0013b46d33715680b671fb22167a0ecf
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-04-07 20:05:46 -0300

    redondeo 2 decimals
    Loop con validacion , hermoso

---
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validInput = true;
+
+            do
+            {
+                decimal numeroDeci;
+
+                Console.WriteLine("Ingrese un numero con decimal para rondear:");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    numeroDeci = Decimal.Parse(input);
+                    validInput = false;
+                    Console.WriteLine($"El numero {input} redondeado es : {Decimal.Round(numeroDeci,2)}");
+                    
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"{input} tiene un formato invalido, ingrese otro numero:"); 
+                }
+
+                
+
+
+            } while (validInput) ;
+            
+            Console.ReadKey(); 
+            
+            
+            
+
+        }
+    }
+}
